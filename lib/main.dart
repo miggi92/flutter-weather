40351b5ef79d6676f7edc454be2f32090e225446
miguel.gebhardt <miguel.gebhardt@googlemail.com>
2024-09-20 13:14:25 +0000

    feat: :sparkles: added city input field

--- lib/main.dart
+++ lib/main.dart
@@ -13,31 +13,37 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Weather',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const MyHomePage(title: 'Weather App'),
+      home: MyHomePage(title: 'Weather App'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  MyHomePage({super.key, required this.title});
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   Map<String, dynamic>? _weatherData;
+  final _cityController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
-    _fetchWeatherData();
+  }
+
+  @override
+  void dispose() {
+    _cityController.dispose();
+    super.dispose();
   }
 
   Future<void> _fetchWeatherData() async {
     try {
-      final weatherData = await getWeatherData('Berlin');
+      final cityName = _cityController.text;
+      final weatherData = await getWeatherData(cityName);
       setState(() {
         _weatherData = weatherData;
       });
@@ -54,23 +60,41 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: _weatherData != null
-            ? Column(
-                // Zeige die Wetterdaten an, wenn sie verfügbar sind
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Temperatur: ${_weatherData!['main']['temp']}°C',
-                    style: Theme.of(context).textTheme.headlineMedium,
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+                  controller: _cityController,
+                  decoration: const InputDecoration(
+                    labelText: 'Stadt',
+                    border: OutlineInputBorder(),
                   ),
-                  Text(
-                    'Beschreibung: ${_weatherData!['weather'][0]['description']}',
-                    style: Theme.of(context).textTheme.headlineSmall,
-                  ),
-                  // Weitere Wetterdaten können hier hinzugefügt werden
-                ],
-              )
-            : const CircularProgressIndicator(), // Zeige einen Ladeindikator, während die Daten geladen werden
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: _fetchWeatherData,
+                  child: const Text('Wetter anzeigen')),
+              if (_weatherData != null)
+                Column(
+                  // Zeige die Wetterdaten an, wenn sie verfügbar sind
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Temperatur: ${_weatherData!['main']['temp']}°C',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    Text(
+                      'Beschreibung: ${_weatherData!['weather'][0]['description']}',
+                      style: Theme.of(context).textTheme.headlineSmall,
+                    ),
+                    // Weitere Wetterdaten können hier hinzugefügt werden
+                  ],
+                ),
+              if (_cityController.text.isNotEmpty && _weatherData == null)
+                const CircularProgressIndicator(),
+            ]),
       ),
     );
   }
