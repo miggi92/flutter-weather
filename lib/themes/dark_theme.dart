508584b7d250b57190ffa099318e1be24ec1658c
miguel.gebhardt <miguel.gebhardt@googlemail.com>
2024-10-08 16:29:42 +0000

    style: :lipstick: updated themes

--- lib/themes/dark_theme.dart
+++ lib/themes/dark_theme.dart
@@ -1,10 +1,51 @@
 // themes/dark_theme.dart
+
 import 'package:flutter/material.dart';
 
 ThemeData darkTheme = ThemeData(
-  primarySwatch: Colors.blueGrey,
   brightness: Brightness.dark,
+  primaryColor: Colors.grey[900],
+  scaffoldBackgroundColor: Colors.grey[800],
   appBarTheme: AppBarTheme(
     backgroundColor: Colors.grey[900],
+    titleTextStyle: const TextStyle(
+      color: Colors.white,
+    ),
+    iconTheme: const IconThemeData(
+      color: Colors.white,
+    ),
+  ),
+  textTheme: TextTheme(
+    headlineMedium: TextStyle(
+      color: Colors.white,
+      fontSize: 20.0,
+      fontWeight: FontWeight.bold,
+    ),
+    headlineSmall: TextStyle(
+      color: Colors.grey[300],
+      fontSize: 16.0,
+    ),
+    bodyMedium: TextStyle(
+      color: Colors.grey[300],
+    ),
+  ),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      backgroundColor: Colors.blue[300],
+      foregroundColor: Colors.grey[900],
+    ),
+  ),
+  cardTheme: CardTheme(
+    color: Colors.grey[700],
+  ),
+  inputDecorationTheme: InputDecorationTheme(
+    labelStyle: TextStyle(
+      color: Colors.grey[400],
+    ),
+    border: OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.grey[600]!,
+      ),
+    ),
   ),
 );
--- lib/themes/light_theme.dart
+++ lib/themes/light_theme.dart
@@ -1,10 +1,51 @@
 // themes/light_theme.dart
+
 import 'package:flutter/material.dart';
 
 ThemeData lightTheme = ThemeData(
-  primarySwatch: Colors.blue,
   brightness: Brightness.light,
+  primaryColor: Colors.blue,
+  scaffoldBackgroundColor: Colors.white,
   appBarTheme: const AppBarTheme(
     backgroundColor: Colors.blue,
+    titleTextStyle: TextStyle(
+      color: Colors.white,
+    ),
+    iconTheme: IconThemeData(
+      color: Colors.white,
+    ),
+  ),
+  textTheme: const TextTheme(
+    headlineMedium: TextStyle(
+      color: Colors.black,
+      fontSize: 20.0,
+      fontWeight: FontWeight.bold,
+    ),
+    headlineSmall: TextStyle(
+      color: Colors.grey,
+      fontSize: 16.0,
+    ),
+    bodyMedium: TextStyle(
+      color: Colors.black,
+    ),
+  ),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      backgroundColor: Colors.blue,
+      foregroundColor: Colors.white,
+    ),
+  ),
+  cardTheme: const CardTheme(
+    color: Colors.white,
+  ),
+  inputDecorationTheme: const InputDecorationTheme(
+    labelStyle: TextStyle(
+      color: Colors.black,
+    ),
+    border: OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.grey,
+      ),
+    ),
   ),
 );
